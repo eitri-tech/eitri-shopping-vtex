0d992ab581e71faf67f099c24e9a1547be452c95
Wagner Quirino <wagner.quirino@calindra.com.br>
2025-01-24 14:52:58 -0300

    Melhorias no código

---
+++ eitri-shopping-vtex-checkout/eitri-app.conf.js
@@ -0,0 +1,21 @@
+module.exports = {
+	'name': 'eitri-shopping-demo-checkout',
+	'title': 'eitri-shopping-demo-checkout',
+	'slug': 'eitri-shopping-demo-checkout',
+	'organization': {},
+	'id': '640b476e-305c-4385-9b4b-ded08895119e',
+	'public-key': 'd11945b3-5aed-462e-a313-7539a5bbcf4f',
+	'organizationId': 'cf5660ee-bf90-42cd-9a43-9d2c69ee3c89',
+	'applicationId': '348850cf-a0f1-4ca1-96e0-6b7db92f4bd9',
+	'type': 'module',
+	'version': '0.2.9',
+	'messageVersion': 'Preços Internacionalização',
+	'eitri-luminus': '1.65.5',
+	'eitri-bifrost': '2.9.0',
+	'eitri-commons': '2.1.2',
+	'eitri-app-dependencies': {
+    'eitri-shopping-vtex-components-shared': { version: '0.2.13', isEitriAppShared: true },
+    'eitri-shopping-vtex-shared': { version: '0.1.15', isEitriAppShared: true },
+    'i18n': { version: '14.1.2', options: { fallbackLng: 'pt' } }
+	}
+}
